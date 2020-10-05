2bfd4dbf857505182956ee0fda6a3c7445a6f3f0
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-05 22:39:25 +0900

    adder csv-parser and code to read the file

--- populatron.js
+++ populatron.js
@@ -1,5 +1,28 @@
 const fs = require("fs");
+const csv = require("csv-parser");
+
+const filepath = "./cities.csv";
+
+async function calculate() {
+  let popArr = [];
+  fs.createReadStream(filepath)
+    .pipe(csv())
+    .on("data", (row) => {
+      popArr.push(row.population);
+    })
+    .on(
+      "end",
+      () =>
+        (popArr = popArr.reduce(
+          (accumulator, currentValue) => accumulator + currentValue
+        ))
+    );
+  return Promise.resolve(popArr);
+}
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    onFinished = calculate();
+    return onFinished.then((arr) => arr.reduce((a, b) => a + b));
+  },
 };
